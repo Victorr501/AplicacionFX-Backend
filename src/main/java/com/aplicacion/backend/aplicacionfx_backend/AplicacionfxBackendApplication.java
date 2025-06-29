e96c00cc33e9c086158b0fd1447c36ed8b94e3e3
victor <vrubinr501@gmail.com>
2025-06-29 13:38:11 +0200

    29/06/2025 13:38

--- src/main/java/com/aplicacion/backend/aplicacionfx_backend/AplicacionfxBackendApplication.java
+++ src/main/java/com/aplicacion/backend/aplicacionfx_backend/AplicacionfxBackendApplication.java
@@ -8,13 +8,8 @@ public class AplicacionfxBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacionfxBackendApplication.class, args);
-		prueba();
-	}
-
-	public static void prueba(){
 
 	}
-
 
 
 }
