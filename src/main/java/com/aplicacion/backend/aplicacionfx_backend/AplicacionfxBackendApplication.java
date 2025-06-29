e9120a8178a79533fff102a930a54fc30a3640f9
victor <vrubinr501@gmail.com>
2025-06-29 13:30:29 +0200

    29/06/2025 13:30

---
+++ src/main/java/com/aplicacion/backend/aplicacionfx_backend/AplicacionfxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacion.backend.aplicacionfx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionfxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionfxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicacion/backend/aplicacionfx_backend/AplicacionfxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacion.backend.aplicacionfx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionfxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
